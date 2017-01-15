7444a7dadab3ccd5f90db4693c1a4f8ecb0bd96c
kazu-megane <kazukazu.music1214@gmail.com>
2017-01-15 22:09:54 +0900

    LINE_UI作ったのをpush

--- smart_schedule/line/event_handler.py
+++ smart_schedule/line/event_handler.py
@@ -1,18 +1,32 @@
-from linebot import LineBotApi
+# -*- coding: utf-8 -*-
 
+from datetime import datetime
+
+from flask import Flask, request, abort
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError,LineBotApiError
+)
 from linebot.models import (
-    MessageEvent, TextMessage, TemplateSendMessage,
-    PostbackTemplateAction, MessageTemplateAction, URITemplateAction, ButtonsTemplate,
-    PostbackEvent
+    MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage,
+    PostbackEvent,StickerSendMessage)
+
+from smart_schedule.line.parts.module import (
+exit_confirm, post_carousel, buttons
 )
 
-import datetime
-
 from smart_schedule.settings import line_env
-from smart_schedule.google_calendar import api_manager
+
+app = Flask(__name__)
 
 line_bot_api = LineBotApi(line_env['channel_access_token'])
 
+flag = False
+
+day_flag = False
+up_day_flag = False
 
 def handle(handler, body, signature):
     handler.handle(body, signature)
@@ -20,37 +34,70 @@ def handle(handler, body, signature):
     @handler.add(MessageEvent, message=TextMessage)
     def handle_message(event):
         print(event)
+        time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        global up_day_flag
+        global day_flag
+        global flag
+
+        if day_flag:
+            day_flag = False
+            reply_text = "{}日後の予定を表示します".format(event.message.text)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=reply_text)
+            )
+            return -1
+
+        if up_day_flag:
+            up_day_flag = False
+            reply_text = "{}日後までの予定を表示します".format(event.message.text)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=reply_text)
+            )
+            return -1
+
+        if flag:
+            flag = False
+            reply_text="キーワードは、{}".format(event.message.text)
+            # keyword = event.message.text.split('、')
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=reply_text)
+            )
+            return -1
+        if event.message.text=="#menu":
+            post_carousel(event.reply_token)
+            return -1
         if not event.message.text.startswith("予定 "):
+            if event.message.text.startswith("大好き"):
+                reply_text = "大好きだよ！！！".format(event.message.text)
+            elif event.message.text.startswith("退出") and not event.source.type=="user":
+                confirm_message = TemplateSendMessage(
+                    alt_text='Confirm template',
+                    template=exit_confirm(time)
+                )
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    confirm_message
+                )
+                return -1
+            else:
+                reply_text=event.message.text
+
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=reply_text)
+            )
             return -1
 
         schedule_name = event.message.text.split(maxsplit=1)[1]
         buttons_template_message = TemplateSendMessage(
             alt_text='Buttons template',
-            template=ButtonsTemplate(
-                # 芝刈り機のイラスト
-                thumbnail_image_url='https://2.bp.blogspot.com/-SObo8z0Ajyw/V9ppuyMxT2I/AAAAAAAA9xI/jwNeixWhDeMJ6K_z96edB45umM6WTftVQCLcB/s800/kusakari_shibakari.png',
-                title="{} の予定".format(schedule_name),
-                text="選択してね",
-                actions=[
-                    PostbackTemplateAction(
-                        label='参加する',
-                        data='join'
-                    ),
-                    PostbackTemplateAction(
-                        label='参加しない',
-                        data='nojoin'
-                    ),
-                    MessageTemplateAction(
-                        label='うっひょおお！！！',
-                        text='うっひょおお！！！'
-                    ),
-                    URITemplateAction(
-                        label='uri',
-                        uri='http://example.com/'
-                    )
-                ]
-            )
+            template=buttons(schedule_name, time)
         )
+        print(buttons_template_message)
+        # text_send_message = TextSendMessage(text=reply_text)
         line_bot_api.reply_message(
             event.reply_token,
             buttons_template_message
@@ -59,21 +106,66 @@ def handle(handler, body, signature):
     @handler.add(PostbackEvent)
     def handle_postback(event):
         print("postbackevent: {}".format(event))
-        credentials = api_manager.get_credentials()
-        if credentials is False:
-            print("Google Calendarの認証をしてね")
-            return
-        service = api_manager.build_service(credentials)
-
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
-        eventsResult = service.events().list(
-            calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
-            orderBy='startTime').execute()
-        events = eventsResult.get('items', [])
-
-        if not events:
-            print('No upcoming events found.')
-        for event in events:
-            start = event['start'].get('dateTime', event['start'].get('date'))
-            print(start, event['summary'])
+        data = event.postback.data.split(',')
+        print(data)
+        print(data[1])
+        pre_time = datetime.strptime(data[1],'%Y-%m-%d %H:%M:%S')
+        compare = datetime.now()-pre_time
+        print(compare)
+        if(compare.total_seconds() < 20):
+            if data[0]=="yes" and event.source.type=="group":
+                try:
+                    line_bot_api.leave_room(event.source.group_id)
+                except LineBotApiError as e:
+                    print("eroor")
+            elif data[0]=="yes" and event.source.type=="room":
+                print("OK")
+                try:
+                    line_bot_api.reply_message(
+                        event.reply_token,
+                        StickerSendMessage(package_id="2" ,sticker_id="42")
+                    )
+                    line_bot_api.leave_room(event.source.room_id)
+                except LineBotApiError as e:
+                    print("eroor")
+            elif data[0]=="no":
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text="退出をキャンセルします。")
+                )
+            elif data[0]=="#keyword_search":
+                global flag
+                flag = True
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text="キーワードを入力してください\n例：バイト、研究室")
+                )
+            elif data[0]=="#after n days_schedule":
+                global day_flag
+                day_flag=True
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    [
+                        TextSendMessage(text="n日後の予定を表示します"),
+                        TextSendMessage(text="何日後の予定を表示しますか？\n例：5")
+                    ]
+                )
+            elif data[0]=="#up to n days_schedule":
+                global up_day_flag
+                up_day_flag=True
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    [
+                        TextSendMessage(
+                            text="n日後までの予定を表示します"
+                        ),
+                        TextSendMessage(
+                            text="何日後までの予定を表示しますか？\n例：5"
+                        )
+                    ]
+                )
+        else:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="タイムアウトです。\nもう一度メニューを開いてからアクションを実行してください")
+            )
---
+++ smart_schedule/models.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, Unicode, UnicodeText, ForeignKey, Boolean
+from sqlalchemy.orm import relationships,backref
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class Personal(db.Model):
+    """
+    個人情報格納のモデル
+    """
+    __tablename__ = "personal_info"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Unicode(255))
+    credential = Column(Unicode(1200))
+    up_to_day_flag = Column(Boolean)
+    day_flag = Column(Boolean)
+    keyword_flag = Column(Boolean)
+
+    #生成された時に呼び出される
+    def __init__(self, user_id, credential):
+        self.user_id = user_id.title()
+        self.credential = credential.title()
+        self.up_to_day_flag = False
+        self.day_flag = False
+        self.keyword_flag = False
