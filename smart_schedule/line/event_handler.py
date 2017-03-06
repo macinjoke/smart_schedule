6f36235a8bbdad3a7255b2fd37603d407c85da04
Shunji Makino <shunji.makino@gmail.com>
2017-03-06 18:48:38 +0900

    refactor about join_message syntax

--- smart_schedule/line/event_handler.py
+++ smart_schedule/line/event_handler.py
@@ -39,10 +39,10 @@ def handle(handler, body, signature):
     @handler.add(JoinEvent)
     def handle_join(event):
         print(event)
-        join_message = 'グループに招待ありがとうございます！\n' \
-                       'グループでは「予定調整機能」「グループに登録されたカレンダーの予定確認」ができます。\n' \
-                       '詳しい使い方はアカウント紹介ページを見てください。\n' \
-                       'グループで使用できるコマンドを呼び出すメッセージは「help」と送信すると見ることができます。'
+        join_message = '''グループに招待ありがとうございます！
+グループでは「予定調整機能」「グループに登録されたカレンダーの予定確認」ができます。
+詳しい使い方はアカウント紹介ページを見てください。
+グループで使用できるコマンドを呼び出すメッセージは「help」と送信すると見ることができます。'''
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=join_message)
