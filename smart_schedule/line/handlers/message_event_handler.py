af57cf54368157a65660f5a7ca908d8b17fc7985
Shunji Makino <shunji.makino@gmail.com>
2017-04-23 12:26:33 +0900

    delete adjust_falg and group_user

--- smart_schedule/line/handlers/message_event_handler.py
+++ smart_schedule/line/handlers/message_event_handler.py
@@ -14,7 +14,7 @@ from smart_schedule.settings import (
     messages, Session, REFRESH_ERROR
 )
 from smart_schedule.google_calendar import api_manager
-from smart_schedule.models import Personal, GroupUser, FreeDay
+from smart_schedule.models import Talk, FreeDay
 
 from . import (
     line_bot_api, reply_google_auth_message, reply_refresh_error_message,
@@ -84,33 +84,18 @@ class MessageEventHandler:
                 buttons_template_message
             )
 
-        @self._add_case(pattern=r'メンバー登録', type=['group', 'room'])
-        def member_register(event, service):
-            username = event.message.text.split(maxsplit=1)[1]
-            talk_id = self._get_talk_id(event)
-            with self.session.begin():
-                person = self.session.query(Personal).filter(
-                    Personal.user_id == talk_id
-                ).one()
-                self.session.add(GroupUser(name=username, group_id=person.id))
-            reply_text = '{}をグループのメンバーに登録しました'.format(username)
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=reply_text)
-            )
-
         @self._add_case(text='select')
         def select(event, service):
             talk_id = self._get_talk_id(event)
             with self.session.begin():
-                person = self.session.query(Personal).filter(
-                    Personal.user_id == talk_id
+                talk = self.session.query(Talk).filter(
+                    Talk.talk_id == talk_id
                 ).one()
-                person.calendar_select_flag = True
+                talk.calendar_select_flag = True
                 try:
                     calendar_list = api_manager.get_calendar_list(service)
                 except client.HttpAccessTokenRefreshError:
-                    self.session.delete(person)
+                    self.session.delete(talk)
                     reply_invalid_credential_error_message(event)
                     return
             reply_text = 'Google Calendar で確認できるカレンダーの一覧です。\n 文字を入力してカレンダーを選択してください'
@@ -136,9 +121,90 @@ class MessageEventHandler:
                 confirm_message
             )
 
+        @self._add_case(
+            pattern=r'[^\s\d]\S*(?:\s(?:[1-9]|1[0-2])/(?:[1-9]|[1-2][0-9]|3[0-1]))+$'
+        )
+        def add_free_day(event, service):
+            talk_id = self._get_talk_id(event)
+            with self.session.begin():
+                talk = self.session.query(Talk).filter(
+                    Talk.talk_id == talk_id
+                ).one()
+            split_message = event.message.text.split()
+            name = split_message[0]
+            day_strs = split_message[1:]
+            datetimes = [
+                datetime.strptime(day_str, '%m/%d') for day_str in day_strs
+            ]
+            # TODO 2017 にしちゃってるけどどうにかしないと1年後使えねえや笑
+            # TODO 無効な日にち（31を指定したが、その月の31日が存在しない場合など)のエラーハンドリングやメッセージを実装する(ValueError)
+            dates = [
+                date(
+                    2017, datetime.month, datetime.day
+                ) for datetime in datetimes
+            ]
+            free_days = [FreeDay(date, name, talk.id) for date in dates]
+            with self.session.begin():
+                # TODO 既にDBにある日を登録しようとしたときのエラーハンドリング及びメッセージの実装(sqlalchemy.exc.IntegrityError)
+                self.session.add_all(free_days)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text='空いている日を保存しました')
+            )
+
+        @self._add_case(text='end')
+        def end(event, service):
+            talk_id = self._get_talk_id(event)
+            with self.session.begin():
+                talk = self.session.query(Talk).filter(
+                    Talk.talk_id == talk_id
+                ).one()
+
+            if len(talk.free_days) == 0:
+                # TODO 登録されている空いている日は無いことを知らせるメッセージが欲しい
+                print('空いている日が登録されてないぞー')
+                return
+            time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            reply_text = '空いている日'
+            dates = [free_day.date for free_day in talk.free_days]
+            date_count_dict = OrderedDict(
+                sorted(Counter(dates).items(), key=lambda x: x[0])
+            )
+            for i, dic in enumerate(date_count_dict.items()):
+                d, count = dic
+                if i % 3 == 0:
+                    reply_text += '\n'
+                else:
+                    reply_text += ', '
+                reply_text += '{}/{} {}人'.format(d.month, d.day, count)
+
+            best_date_count = max(date_count_dict.values())
+            best_dates = [
+                k for k, v in date_count_dict.items()
+                if v == best_date_count
+            ]
+            print(len(reply_text))
+            buttons_template_message = TemplateSendMessage(
+                alt_text='Button template',
+                # TODO ボタンテンプレートが4つしか受け付けないので4つしか選べない
+                template=EventCreateButtons(
+                    time,
+                    messages['templates']['event_create_buttons'],
+                    reply_text,
+                    best_dates[:4]
+                )
+            )
+            line_bot_api.reply_message(
+                event.reply_token,
+                buttons_template_message
+            )
+            with self.session.begin():
+                for free_day in talk.free_days:
+                    self.session.delete(free_day)
+
         @self._add_flag_case(flag='day_flag')
-        def day(event, service, person):
-            person.day_flag = False
+        def day(event, service, talk):
+            talk.day_flag = False
             try:
                 days = int(event.message.text)
             except ValueError:
@@ -146,10 +212,10 @@ class MessageEventHandler:
                 return
             try:
                 events = api_manager.get_events_after_n_days(
-                    service,  person.calendar_id, days
+                    service,  talk.calendar_id, days
                 )
             except client.HttpAccessTokenRefreshError:
-                self.session.delete(person)
+                self.session.delete(talk)
                 reply_invalid_credential_error_message(event)
                 return
             reply_text = '{}日後の予定'.format(days)
@@ -160,8 +226,8 @@ class MessageEventHandler:
             )
 
         @self._add_flag_case(flag='up_to_day_flag')
-        def up_to_day(event, service, person):
-            person.up_to_day_flag = False
+        def up_to_day(event, service, talk):
+            talk.up_to_day_flag = False
             try:
                 days = int(event.message.text)
             except ValueError:
@@ -169,10 +235,10 @@ class MessageEventHandler:
                 return
             try:
                 events = api_manager.get_n_days_events(
-                    service,  person.calendar_id, days
+                    service,  talk.calendar_id, days
                 )
             except client.HttpAccessTokenRefreshError:
-                self.session.delete(person)
+                self.session.delete(talk)
                 reply_invalid_credential_error_message(event)
                 return
             reply_text = '{}日後までの予定'.format(days)
@@ -183,15 +249,15 @@ class MessageEventHandler:
             )
 
         @self._add_flag_case(flag='keyword_flag')
-        def keyword(event, service, person):
-            person.keyword_flag = False
+        def keyword(event, service, talk):
+            talk.keyword_flag = False
             keyword = event.message.text
             try:
                 events = api_manager.get_events_by_title(service,
-                                                         person.calendar_id,
+                                                         talk.calendar_id,
                                                          keyword)
             except client.HttpAccessTokenRefreshError:
-                self.session.delete(person)
+                self.session.delete(talk)
                 reply_invalid_credential_error_message(event)
                 return
             reply_text = '{}の検索結果'.format(keyword)
@@ -203,118 +269,33 @@ class MessageEventHandler:
             )
 
         @self._add_flag_case(flag='calendar_select_flag')
-        def calendar_select(event, service, person):
+        def calendar_select(event, service, talk):
             try:
                 calendar_list = api_manager.get_calendar_list(service)
             except client.HttpAccessTokenRefreshError:
-                self.session.delete(person)
+                self.session.delete(talk)
                 reply_invalid_credential_error_message(event)
                 return
             summaries = [item['summary'] for item in calendar_list['items']]
             if event.message.text in summaries:
-                person.calendar_select_flag = False
+                talk.calendar_select_flag = False
                 calendar_id = [
                     item['id'] for item in calendar_list['items']
                     if item['summary'] == event.message.text
                 ][0]
-                person.calendar_id = calendar_id
+                talk.calendar_id = calendar_id
                 reply_text = 'カレンダーを {} に設定しました'.format(event.message.text)
                 line_bot_api.reply_message(
                     event.reply_token,
                     TextSendMessage(text=reply_text)
                 )
             else:
-                person.calendar_select_flag = False
+                talk.calendar_select_flag = False
                 reply_text = '{} はカレンダーには存在しません'.format(event.message.text)
                 line_bot_api.reply_message(
                     event.reply_token,
                     TextSendMessage(text=reply_text)
                 )
-
-        @self._add_flag_case(flag='adjust_flag')
-        def adjust(event, service, person):
-            group_users = self.session.query(GroupUser).filter(
-                GroupUser.group_id == person.id
-            ).all()
-            # グループのメンバーをシステムに登録していなかった場合
-            if len(group_users) == 0:
-                reply_text = 'グループのメンバーを登録してください\n例：メンバー登録 橋本'
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text=reply_text)
-                )
-                person.adjust_flag = False
-                return -1
-
-            # グループの予定調整を終了
-            if event.message.text == "end":
-                time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                reply_text = '空いている日'
-                dates = []
-                for group_user in group_users:
-                    dates.extend([free_day.date for free_day in group_user.free_days])
-                    for free_day in group_user.free_days:
-                        self.session.delete(free_day)
-
-                date_count_dict = OrderedDict(
-                    sorted(Counter(dates).items(), key=lambda x: x[0])
-                )
-                for i, dic in enumerate(date_count_dict.items()):
-                    d, count = dic
-                    if i % 3 == 0:
-                        reply_text += '\n'
-                    else:
-                        reply_text += ', '
-                    reply_text += '{}/{} {}人'.format(d.month, d.day, count)
-
-                best_date_count = max(date_count_dict.values())
-                best_dates = [
-                    k for k, v in date_count_dict.items()
-                    if v == best_date_count
-                ]
-                print(len(reply_text))
-                buttons_template_message = TemplateSendMessage(
-                    alt_text='Button template',
-                    # TODO ボタンテンプレートが4つしか受け付けないので4つしか選べない
-                    template=EventCreateButtons(
-                        time,
-                        messages['templates']['event_create_buttons'],
-                        reply_text,
-                        best_dates[:4]
-                    )
-                )
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    buttons_template_message
-                )
-                person.adjust_flag = False
-                return -1
-
-            user_names = tuple(group_user.name for group_user in group_users)
-            if event.message.text.startswith(user_names):
-                split_message = event.message.text.split()
-                name = split_message[0]
-                group_user = [
-                    group_user for group_user in group_users
-                    if group_user.name == name
-                ][0]
-                day_strs = split_message[1:]
-                datetimes = [
-                    datetime.strptime(day_str, '%m/%d') for day_str in day_strs
-                ]
-                # TODO 2017 にしちゃってるけどどうにかしないと1年後使えねえや笑
-                dates = [
-                    date(
-                        2017, datetime.month, datetime.day
-                    ) for datetime in datetimes
-                ]
-                free_days = [FreeDay(date, group_user.id) for date in dates]
-                self.session.add_all(free_days)
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text='空いている日を保存しました')
-                )
-                return -1
 
         @self.handler.add(MessageEvent, message=TextMessage)
         def handle(event):
@@ -336,16 +317,18 @@ class MessageEventHandler:
 
             for func, flag in self.flag_cases:
                 talk_id = self._get_talk_id(event)
-                # TODO トランザクション開始とコミットのタイミングこれじゃまずい気がする
+                # TODO トランザクション開始とコミットのタイミングこれじゃまずい
                 self.session.begin()
-                person = self.session.query(Personal).filter(
-                    Personal.user_id == talk_id
+                talk = self.session.query(Talk).filter(
+                    Talk.talk_id == talk_id
                 ).one()
-                if getattr(person, flag):
-                    func(event, service, person)
-                    self.session.commit()
-                    return
-                self.session.close()
+                try:
+                    if getattr(talk, flag):
+                        func(event, service, talk)
+                        self.session.commit()
+                        return
+                finally:
+                    self.session.close()
 
             for func, text, type, pattern, pattern_flags in self.cases:
                 if self._validate(event, text, type, pattern, pattern_flags):
@@ -378,7 +361,7 @@ class MessageEventHandler:
         """A decorator that is used to register a function executing 
         in case of erecting given flags.
 
-        :param flag: the flag of `smart_schedule.models.Personal` as string
+        :param flag: the flag of `smart_schedule.models.Talk` as string
         """
 
         def wrapper(func):
